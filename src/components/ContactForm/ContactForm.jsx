0335ac103a061aa300f837c22fb7eae41fa04a56
TomaszAndrzejPawlowski <pawlowski.t.93@gmail.com>
2023-11-20 17:31:44 +0100

    removed unnecessary id, css for buttons

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,4 @@
 import { Component } from 'react';
-import { v4 as uuidv4 } from 'uuid';
 import css from './ContactForm.module.css';
 
 const INITIAL_STATE = {
@@ -9,8 +8,6 @@ const INITIAL_STATE = {
 
 export class ContactForm extends Component {
   state = { ...INITIAL_STATE };
-
-  generateUuid = uuidv4();
 
   handleChange = name => e => {
     const { target } = e;
@@ -31,7 +28,7 @@ export class ContactForm extends Component {
   render() {
     return (
       <form className={css.contactForm} onSubmit={this.handleSubmit}>
-        <label htmlFor={this.generateUuid}>Name</label>
+        <label>Name</label>
         <input
           className={css.contactForm__input}
           type="text"
@@ -40,10 +37,9 @@ export class ContactForm extends Component {
           onChange={this.handleChange('name')}
           pattern="^[a-zA-Zа-яА-Я]+([ -'][a-zA-Zа-яА-Я]+)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          id={this.generateUuid}
           required
         />
-        <label htmlFor={this.generateUuid}>Number </label>
+        <label>Number</label>
         <input
           className={css.contactForm__input}
           type="tel"
@@ -52,10 +48,8 @@ export class ContactForm extends Component {
           onChange={this.handleChange('number')}
           pattern="^[+]?[0-9 \u0028\u0029\u002D]*$"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          id={this.generateUuid}
           required
         />
-
         <button type="submit">Add contact</button>
       </form>
     );
